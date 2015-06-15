b0de4c6f7129b47451c8d7e80d695ce895252fdc
BrieflyX <brieflyx@gmail.com>
2015-06-15 22:50:08 +0800

    fix the path bug

--- centos/install-ss.sh
+++ centos/install-ss.sh
@@ -9,6 +9,6 @@ yum install python-setuptools m2crypto supervisor &> /dev/null
 easy_install pip &> /dev/null
 pip install shadowsocks &> /dev/null
 
-cp ../shadowsocks.json /etc/shadowsocks.json
-cp ../ss-supervisor.conf /etc/supervisor/conf.d/ss.conf
+cp shadowsocks.json /etc/shadowsocks.json
+cp ss-supervisor.conf /etc/supervisor/conf.d/ss.conf
 service supervisord start
--- debian/compiler-tb.sh
+++ debian/compiler-tb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 apt-get install gcc &> /dev/null
-gcc ../tb_userspace.c -l pthread -o ../tb_userspace
-cp ../tb_userspace /usr/local/bin/tb_userspace
+gcc tb_userspace.c -l pthread -o tb_userspace
+cp tb_userspace /usr/local/bin/tb_userspace
